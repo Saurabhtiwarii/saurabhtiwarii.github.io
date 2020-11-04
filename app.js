d972ed58b801d76f2a0f555004ae19e1301bb531
saurabh <sourabhkt19@gmail.com>
2020-11-04 17:32:41 +0530

    section4 changes

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ window.addEventListener('scroll',function(){
 ScrollOut({
     targets : 'section,div,button,a,nav'
 })
+// project1 open
+const sec1 = ()=> {
+    const project1 = document.querySelector('.project1');
+    project1.addEventListener('click', ()=> {
+        project1.classList.toggle('open_sec1');
+        })
+}
+sec1();
+// project1 close
+const pr1_close = ()=>{
+    const close_btn1 =  document.querySelector(".close_btn1");
+    const project1 = document.querySelector('.project1');
+    close_btn1.addEventListener('click', ()=> {
+        project1.classList.remove('open_sec1');
+        })
+}
