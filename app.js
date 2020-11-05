0ba44d3c199ac8608d39ec701b9ce0307ba67c47
saurabh <sourabhkt19@gmail.com>
2020-11-05 20:29:50 +0530

    section 4 fixes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// hamburger active
 const nav = ()=> {
     const hamburger = document.querySelector('.hamburger');
     const navlinks = document.querySelector('.nav_links');
@@ -16,19 +17,71 @@ window.addEventListener('scroll',function(){
 ScrollOut({
     targets : 'section,div,button,a,nav'
 })
-// project1 open
+// project1 active
 const sec1 = ()=> {
-    const project1 = document.querySelector('.project1');
+    const project = document.querySelector('.projects');
+    const project1= document.querySelector('.project1');
     project1.addEventListener('click', ()=> {
-        project1.classList.toggle('open_sec1');
+        project.classList.toggle('pr1_active');
         })
 }
 sec1();
 // project1 close
-const pr1_close = ()=>{
-    const close_btn1 =  document.querySelector(".close_btn1");
-    const project1 = document.querySelector('.project1');
-    close_btn1.addEventListener('click', ()=> {
-        project1.classList.remove('open_sec1');
+// const pr1_close = ()=>{
+//     const close_btn1 =  document.querySelector(".close_btn1");
+//     const project = document.querySelector('.projects');
+//     close_btn1.addEventListener('click', ()=> {
+//         project.classList.remove('pr1_active');
+//         })
+// }
+// pr1_close();
+
+// project 2 active
+const pr2 = ()=> {
+    const project = document.querySelector('.projects');
+    const project2= document.querySelector('.project2');
+    project2.addEventListener('click', ()=> {
+        project.classList.toggle('pr2_active');
         })
 }
+pr2();
+
+//**  Project3 active //
+const pr3 = ()=> {
+    const project = document.querySelector('.projects');
+    const project3= document.querySelector('.project3');
+    project3.addEventListener('click', ()=> {
+        project.classList.toggle('pr3_active');
+        })
+}
+pr3();
+
+//**  Project4 active //
+const pr4 = ()=> {
+    const project = document.querySelector('.projects');
+    const project4= document.querySelector('.project4');
+    project4.addEventListener('click', ()=> {
+        project.classList.toggle('pr4_active');
+        })
+}
+pr4();
+
+//**  Project5 active //
+const pr5 = ()=> {
+    const project = document.querySelector('.projects');
+    const project5= document.querySelector('.project5');
+    project5.addEventListener('click', ()=> {
+        project.classList.toggle('pr5_active');
+        })
+}
+pr5();
+//**  Project6 active //
+const pr6 = ()=> {
+    const project = document.querySelector('.projects');
+    const project5= document.querySelector('.project6');
+    project5.addEventListener('click', ()=> {
+        project.classList.toggle('pr6_active');
+        })
+}
+pr6();
+
