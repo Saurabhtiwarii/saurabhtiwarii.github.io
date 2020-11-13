bb895e6e46f1074f082048139a87d9a798257893
saurabh <sourabhkt19@gmail.com>
2020-11-13 19:47:31 +0530

    pr1 fixes

--- app.js
+++ app.js
@@ -15,73 +15,10 @@ window.addEventListener('scroll',function(){
 })
 // scroll animation
 ScrollOut({
-    targets : 'section,div,button,a,nav'
+   
+    targets : 'section,div,button,a,nav',
+    once:true,
+    threshold:0.5
 })
 // project1 active
-const sec1 = ()=> {
-    const project = document.querySelector('.projects');
-    const project1= document.querySelector('.project1');
-    project1.addEventListener('click', ()=> {
-        project.classList.toggle('pr1_active');
-        })
-}
-sec1();
-// project1 close
-// const pr1_close = ()=>{
-//     const close_btn1 =  document.querySelector(".close_btn1");
-//     const project = document.querySelector('.projects');
-//     close_btn1.addEventListener('click', ()=> {
-//         project.classList.remove('pr1_active');
-//         })
-// }
-// pr1_close();
-
-// project 2 active
-const pr2 = ()=> {
-    const project = document.querySelector('.projects');
-    const project2= document.querySelector('.project2');
-    project2.addEventListener('click', ()=> {
-        project.classList.toggle('pr2_active');
-        })
-}
-pr2();
-
-//**  Project3 active //
-const pr3 = ()=> {
-    const project = document.querySelector('.projects');
-    const project3= document.querySelector('.project3');
-    project3.addEventListener('click', ()=> {
-        project.classList.toggle('pr3_active');
-        })
-}
-pr3();
-
-//**  Project4 active //
-const pr4 = ()=> {
-    const project = document.querySelector('.projects');
-    const project4= document.querySelector('.project4');
-    project4.addEventListener('click', ()=> {
-        project.classList.toggle('pr4_active');
-        })
-}
-pr4();
-
-//**  Project5 active //
-const pr5 = ()=> {
-    const project = document.querySelector('.projects');
-    const project5= document.querySelector('.project5');
-    project5.addEventListener('click', ()=> {
-        project.classList.toggle('pr5_active');
-        })
-}
-pr5();
-//**  Project6 active //
-const pr6 = ()=> {
-    const project = document.querySelector('.projects');
-    const project5= document.querySelector('.project6');
-    project5.addEventListener('click', ()=> {
-        project.classList.toggle('pr6_active');
-        })
-}
-pr6();
 
